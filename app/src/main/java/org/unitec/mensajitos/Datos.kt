a8b690d116f82670a4fd23943ffdf3b677b4cec3
campitos <rapidclimate@gmail.com>
2018-11-16 18:29:46 -0600

    Initial commit

---
+++ app/src/main/java/org/unitec/mensajitos/Datos.kt
@@ -0,0 +1,7 @@
+package org.unitec.mensajitos
+
+class Datos {
+    var name: String? = null
+    var emailaddress: String? = null
+    var subject: String? = null
+}
---
+++ app/src/main/java/org/unitec/mensajitos/Estatus.kt
@@ -0,0 +1,12 @@
+package org.unitec.mensajitos
+
+/**
+ * Created by rapid on 08/12/2017.
+ */
+
+class Estatus {
+     var success: Boolean = false
+     var mensaje: String? = null
+
+
+}
