5478b6aae3ece0c71fa16c416593bda0a8d54bf0
campitos <rapidclimate@gmail.com>
2018-11-23 21:12:39 -0600

    fifis

--- app/src/main/java/org/unitec/mensajitos/MainActivity.kt
+++ app/src/main/java/org/unitec/mensajitos/MainActivity.kt
@@ -6,28 +6,42 @@ import android.os.Bundle
 import android.widget.Toast
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.android.synthetic.main.activity_main.*
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
 class MainActivity : AppCompatActivity() {
 var mensajes=ArrayList<Mensaje>()
     var mensaje="hola mundo"
+    var mensa=Mensaje();
+    var miid="";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TareaMensajes().execute(null,null,null)
+
+           boton.setOnClickListener {
+               TareaMensajes().execute(null,null,null)
+           }
+
 
 
     }
 
     inner  class TareaMensajes :AsyncTask<String,String,String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+              miid=   textoId.text.toString();
+
+            //miid="5bb42224b1ff2b18b3c139be"
+        }
 
         override fun doInBackground(vararg p0: String?): String {
             //Aqui dentroa vamos a establecer nuestra conexion al back end
             //pero no podemos mostrar nada en las componentes visuales
             //Primero la url
-            var url="https://topoyiyo.herokuapp.com/api/mensaje"
+            var url="https://topoyiyo.herokuapp.com/api/mensaje/"+miid
 
             //Generamos un objeto de la clase RestTemplate
             var rest=RestTemplate()
@@ -42,18 +56,18 @@ var mensajes=ArrayList<Mensaje>()
             //tipo de referncia arralist generico a mensajes
             var maper=ObjectMapper()
             //ya convertimos
-  mensajes= maper.readValue(valor,object :TypeReference<ArrayList<Mensaje>>(){})
+  //mensajes= maper.readValue(valor,object :TypeReference<ArrayList<Mensaje>>(){})
+            mensa= maper.readValue(valor,object :TypeReference<Mensaje>(){})
 
 
             return "mensajes"
         }
 
+
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            // Depues del doInbackground la ejecucion de este thread pasa a este
-            //metodo el cuañ si puede enlazasre a componentes visuales
-           Toast.makeText(applicationContext,"Mensajes hallados "+mensajes.size,
-               Toast.LENGTH_LONG).show()
+
+            Toast.makeText(applicationContext,mensa.cuerpo,Toast.LENGTH_LONG).show()
         }
     }
 
