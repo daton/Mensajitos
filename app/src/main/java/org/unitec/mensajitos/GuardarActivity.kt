9582a4dad9a9cd3950f609afe2873155addcd825
campitos <rapidclimate@gmail.com>
2018-11-30 21:10:59 -0600

    fifis

---
+++ app/src/main/java/org/unitec/mensajitos/GuardarActivity.kt
@@ -0,0 +1,12 @@
+package org.unitec.mensajitos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GuardarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guardar)
+    }
+}
