b41f68d64c7e4bc5b3afaf7951cee6ba0c5bc46f
campitos <rapidclimate@gmail.com>
2018-11-30 21:37:19 -0600

    fifis

--- app/src/main/java/org/unitec/mensajitos/GuardarActivity.kt
+++ app/src/main/java/org/unitec/mensajitos/GuardarActivity.kt
@@ -1,12 +1,37 @@
 package org.unitec.mensajitos
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_guardar.*
 
 class GuardarActivity : AppCompatActivity() {
+   var mensa=Mensaje();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guardar)
+    }
+
+    inner class TareaGuardar :AsyncTask<String,String,String>(){
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+
+            return "hola"
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            //pedimos el valor del titulo del mensaje
+           mensa.titulo= textoMiTitulo.text.toString()
+            //Pedimos en valor al cuerpo del mensaje;
+            mensa.cuerpo=textoMiCuerpo.text.toString();
+            //Construir un objeto de tipo mensaje a partir de estos valores
+
+
+        }
     }
 }
